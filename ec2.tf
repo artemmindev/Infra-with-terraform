7cdfc986c4620c6b26a760bd2be2f1048e87be61
artemmindev <artemmindev@outlook.com>
2024-11-14 01:33:28 +0100

    add ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "my-app" {
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  key_name      = "devops"
+  subnet_id     = aws_subnet.public-eu-central-1a.id
+
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.my-app-sg.id]
+
+  tags = {
+    Name = "my-app"
+  }
+}
