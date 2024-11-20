bfc44f126c9227c98597597f72c1234e5297db96
artemmindev <artemmindev@outlook.com>
2024-11-19 20:53:03 -0500

    add route table

--- subnets.tf
+++ subnets.tf
@@ -18,3 +18,24 @@ resource "aws_subnet" "private-eu-central-1a" {
     "Name" = "private-eu-central-1a"
   }
 }
+
+resource "aws_subnet" "public-eu-central-1b" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.31.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "public-eu-central-1b"
+  }
+}
+
+resource "aws_subnet" "private-eu-central-1b" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.62.0/24"
+  availability_zone = "eu-central-1b"
+
+  tags = {
+    "Name" = "private-eu-central-1b"
+  }
+}
